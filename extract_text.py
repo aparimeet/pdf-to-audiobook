4ed19c6ab154dc25ecd99c6a60cb7467f726bbd9
aparimeet <aparimeet@gmail.com>
2025-04-05 21:09:52 +0530

    Cleanup extract_text.py

--- extract_text.py
+++ extract_text.py
@@ -5,27 +5,32 @@ from PyPDF2 import PdfReader
 def remove_trademark(content: str) -> str:
     return content.replace("OceanofPDF.com", "")
 
-index = None
-with open("index.json", "r") as f:
-    index = json.load(f)
-print(index)
+def get_index(file_path: str) -> dict:
+    index = None
+    with open(file_path, "r") as f:
+        index = json.load(f)
+    print(index)
+    return index
 
-# Open the PDF file (make sure the file path is correct)
-reader = PdfReader("sample.pdf")
-#print(len(reader.pages))
+def extract_organize_pages(pdf_name: str, index_file_name: str) -> None:
+    # Open the PDF file (make sure the file path is correct)
+    reader = PdfReader(pdf_name)
+    print(f"Total number of pages: {len(reader.pages)}")
 
-# Loop over each page and print its text content
-for key, value in index.items():
-    content = ""
-    start = value["start"]
-    end = value["end"]
-    for page in range(start, end+1):
-        content += reader.pages[page].extract_text()
-    #content += '\n'
-    content = remove_trademark(content)
-    with open(f"{key}.txt", "w") as f:
-        f.write(content)
+    index = get_index(index_file_name)
+    # Loop over each page and print its text content
+    for key, value in index.items():
+        content = ""
+        start = value["start"]
+        end = value["end"]
+        for page in range(start, end+1):
+            content += reader.pages[page].extract_text()
+        content = remove_trademark(content)
+        with open(f"{key}.txt", "w") as f:
+            f.write(content)
 
+if __name__ == "__main__":
+    extract_organize_pages("sample.pdf", "index.json")
 
 
 
