3290b5c3d625cf2f0c0f16480909f6d09ea43bbd
aparimeet <aparimeet@gmail.com>
2025-04-11 12:39:00 +0530

    Trying out kokoro

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Project to convert a PDF given by the user to an audiobook.
 
+Python version:
+3.12.1
+
 Install Pytorch:
 https://pytorch.org/
 
--- tts.py
+++ tts.py
@@ -1,9 +1,30 @@
-from transformers import pipeline
-import scipy
+# 3️⃣ Initalize a pipeline
+from kokoro import KPipeline
+from IPython.display import display, Audio
+import soundfile as sf
+import torch
+# 🇺🇸 'a' => American English, 🇬🇧 'b' => British English
+pipeline = KPipeline(lang_code='a') # <= make sure lang_code matches voice, reference above.
 
-synthesiser = pipeline("text-to-speech", "suno/bark")
+# This text is for demonstration purposes only, unseen during training
+text = '''
+The sky above the port was the color of television, tuned to a dead channel.
+'''
+# Generate, display, and save audio files in a loop.
+generator = pipeline(
+    text, voice='af_heart', # <= change voice here
+    speed=1, split_pattern=r'\n+'
+)
+# Alternatively, load voice tensor directly:
+# voice_tensor = torch.load('path/to/voice.pt', weights_only=True)
+# generator = pipeline(
+#     text, voice=voice_tensor,
+#     speed=1, split_pattern=r'\n+'
+# )
 
-speech = synthesiser("Hello, my dog is cooler than you!", forward_params={"do_sample": True})
-
-scipy.io.wavfile.write("bark_out.wav", rate=speech["sampling_rate"], data=speech["audio"])
-
+for i, (gs, ps, audio) in enumerate(generator):
+    print(i)  # i => index
+    print(gs) # gs => graphemes/text
+    print(ps) # ps => phonemes
+    display(Audio(data=audio, rate=24000, autoplay=i==0))
+    sf.write(f'{i}.wav', audio, 24000) # save each audio file
