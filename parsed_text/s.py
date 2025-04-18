7c1c96a5471d187ef174fd057edaa130e9dfd18f
Aparimeet Dakhane <aparimeet@gmail.com>
2025-04-18 19:32:50 +0530

    Update tts.py and remove unused files

--- parsed_text/s.py
+++
@@ -1,6 +0,0 @@
-with open("Prologue_cp.txt", "r") as f:
-    text = f.read()
-ctext = text.replace("\n", "")
-
-with open("Prologue_cp.txt", "w") as f:
-    f.write(ctext)
--- tts.py
+++ tts.py
@@ -3,27 +3,29 @@ from kokoro import KPipeline
 from IPython.display import display, Audio
 import soundfile as sf
 import torch
+import os
+
 # 🇺🇸 'a' => American English, 🇬🇧 'b' => British English
 pipeline = KPipeline(lang_code='a') # <= make sure lang_code matches voice, reference above.
 
-# This text is for demonstration purposes only, unseen during training
-with open("parsed_text/Prologue_cp.txt", "r") as f:
-    text = f.read()
-# Generate, display, and save audio files in a loop.
-generator = pipeline(
-    text, voice='af_heart', # <= change voice here
-    speed=1
-)
-# Alternatively, load voice tensor directly:
-# voice_tensor = torch.load('path/to/voice.pt', weights_only=True)
-# generator = pipeline(
-#     text, voice=voice_tensor,
-#     speed=1, split_pattern=r'\n+'
-# )
+curr_dir = os.getcwd()
+files = os.listdir(curr_dir + "/parsed_text")
+print(f"{files = }")
+for file in files:
+    filename_wo_extension = file.split(".")[0]
+    print(f"Converting {file} to speech")
+    with open(f"{curr_dir}/parsed_text/{file}", "r") as f:
+        text = f.read()
+    # Generate, display, and save audio files in a loop.
+    generator = pipeline(
+        text, voice='af_heart', # <= change voice here
+        speed=1
+    )
 
-for i, (gs, ps, audio) in enumerate(generator):
-    print(i)  # i => index
-    print(gs) # gs => graphemes/text
-    print(ps) # ps => phonemes
-    display(Audio(data=audio, rate=24000, autoplay=i==0))
-    sf.write(f'Prologue/{i}.wav', audio, 24000) # save each audio file
+    os.mkdir(curr_dir + "/audio/" + filename_wo_extension)
+    for i, (gs, ps, audio) in enumerate(generator):
+        print(i)  # i => index
+        print(gs) # gs => graphemes/text
+        print(ps) # ps => phonemes
+        display(Audio(data=audio, rate=24000, autoplay=i==0))
+        sf.write(f'{curr_dir}/audio/{filename_wo_extension}/{i}.wav', audio, 24000) # save each audio file
